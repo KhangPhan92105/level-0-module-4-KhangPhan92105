2b223835f6b50fc0502630a84c6c92241f1d5474
root <root@6cb72c805eda>
2020-08-13 20:44:49 +0000

    PiAloud

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -13,22 +13,48 @@ public static void main(String[] args) {
    String mathHard = "3.141592653589793238462643";
    //
    String englush = "Wudhawdyuhaw";
+   
+   String piMath = "3.1415";
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
    System.out.println(englush.charAt(3));
    
-   System.out.println(mathHard.length());
+   System.out.println(englush.length());
 	// 4. Print ALL the digits of of the Pi String (hint: use a loop)
-   for (int i = 0; i < (englush.length()); i++) {
+   //englush.length = 12
+   //number of characters in englush = 12
+   //Goal -  we want to print out all the chars in englush
+   //0-11 inclusive 12
+   // 11 minus 0 = 11 => this wouldn't get me enough chars WRONG
+   for (int i = 0; i < englush.length(); i++) {
 		System.out.println(englush.charAt(i));
    }
+   
 	// 5. Use the speak() method below to speak each digit of Pi.
-   for (int i = 0; i < (englush.length()); i++) {
+  for (int i = 0; i < (englush.length()); i++) {
 		speak(englush.charAt(i));
   }
 	// [CHALLENGE]
 	// *6. Get a character from the user using the getInputFromUser() method
+   char galaxyBrain = getInputFromUser();
+  System.out.println(galaxyBrain);
+   
+   
 	// *7. Compare the users' char to the next digit of Pi
+   
+   		//Print out the first 5 digits of pi on a single line
+  //this told the user the first 5 digits of pi
+  // System.out.print(piMath);
+   		
+   //GOAL -- Tell the user "Type in the next digit of pi"
+   System.out.print("what is the next digit of Pi?");
+ //Ask the user for the next digit of pi using the getInputFromUser() method
+   char galaxyBrain2 = getInputFromUser();
+  
+   
+   //compare that to your pi variable that you have saved in the program already
+   		//if the user is correct, print out "Correct" otherwise print out back to Bronze V"
+   
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
 	
 }	
@@ -53,8 +79,16 @@ public static void main(String[] args) {
 		}
 	}
 
+	//f(x) = x + 2;
+	// x = 3;
+	//f(x) = 5;
+	//f(x) =  function name
+	// x  = data that we input
+	// 5 = output or return value
+	
 	static char getInputFromUser() {
-		Scanner scanner = new Scanner(System.in);
+		System.out.println("made it here");
+		Scanner scanner = new Scanner(System.in); 
 		return scanner.next().toCharArray()[0];
 	}
 
